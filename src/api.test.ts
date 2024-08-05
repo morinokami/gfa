6d4129fb08e490f60421976425d29e895cb5b1aa
Shinya Fujino <shf0811@gmail.com>
2024-08-05 22:11:23 +0900

    Test each route

--- src/api.test.ts
+++ src/api.test.ts
@@ -1,54 +1,123 @@
 import assert from "node:assert/strict";
 import test from "node:test";
-import { inspectRoutes } from "hono/dev";
-
+import type { Hono } from "hono";
 import { createApi } from "./api";
 
 test("createApi", async (t) => {
-	await t.test("should create an API for a single item", () => {
-		const api = createApi("user", { id: 1, name: "Alice" }, "api");
-		const routes = inspectRoutes(api);
+	await t.test("single item", async (t) => {
+		let api: Hono;
+		t.beforeEach(() => {
+			api = createApi("user", { id: 1, name: "Alice" }, "api");
+		});
 
-		// there are 3 routes
-		assert.strictEqual(routes.length, 3);
-		// route path is correct
-		assert(routes.every((route) => route.path === "/api/user"));
-		// expected methods are present
-		const methods = routes.map((route) => route.method);
-		assert(methods.includes("GET"));
-		assert(methods.includes("PUT"));
-		assert(methods.includes("PATCH"));
+		await t.test("can GET a single item", async () => {
+			const res = await api.request("/api/user", { method: "GET" });
+			assert.deepStrictEqual(await res.json(), { id: 1, name: "Alice" });
+		});
+
+		await t.test("can PUT a single item", async () => {
+			const res = await api.request("/api/user", {
+				method: "PUT",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ id: 2, name: "Bob" }),
+			});
+			assert.strictEqual(res.status, 204);
+
+			const res2 = await api.request("/api/user", { method: "GET" });
+			assert.deepStrictEqual(await res2.json(), { id: 2, name: "Bob" });
+		});
+
+		await t.test("can PATCH a single item", async () => {
+			const res = await api.request("/api/user", {
+				method: "PATCH",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ name: "Bob" }),
+			});
+			assert.strictEqual(res.status, 204);
+
+			const res2 = await api.request("/api/user", { method: "GET" });
+			assert.deepStrictEqual(await res2.json(), { id: 1, name: "Bob" });
+		});
 	});
 
-	await t.test("should create an API for multiple items", () => {
-		const api = createApi(
-			"users",
-			[
+	await t.test("multiple items", async (t) => {
+		let api: Hono;
+		t.beforeEach(() => {
+			api = createApi(
+				"users",
+				[
+					{ id: 1, name: "Alice" },
+					{ id: 2, name: "Bob" },
+				],
+				"api",
+			);
+		});
+
+		await t.test("can GET multiple items", async () => {
+			const res = await api.request("/api/users", { method: "GET" });
+			assert.deepStrictEqual(await res.json(), [
 				{ id: 1, name: "Alice" },
 				{ id: 2, name: "Bob" },
-			],
-			"api",
-		);
-		const routes = inspectRoutes(api);
+			]);
+		});
 
-		// there are 6 routes
-		assert.strictEqual(routes.length, 6);
-		// expected methods are present
-		const routesWithoutId = routes.filter(
-			(route) => route.path === "/api/users",
-		);
-		const routesWithId = routes.filter((route) => route.path !== "/api/users");
-		assert.strictEqual(routesWithoutId.length, 2);
-		assert.strictEqual(routesWithId.length, 4);
-		const methodsWithoutId = routesWithoutId.map((route) => route.method);
-		const methodsWithId = routesWithId.map((route) => route.method);
-		assert(methodsWithoutId.includes("GET"));
-		assert(methodsWithoutId.includes("POST"));
-		assert(methodsWithId.includes("GET"));
-		assert(methodsWithId.includes("PUT"));
-		assert(methodsWithId.includes("PATCH"));
-		assert(methodsWithId.includes("DELETE"));
+		await t.test("can GET a single item", async () => {
+			const res = await api.request("/api/users/1", { method: "GET" });
+			assert.deepStrictEqual(await res.json(), { id: 1, name: "Alice" });
+		});
+
+		await t.test("can POST a single item", async () => {
+			const res = await api.request("/api/users", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ id: 3, name: "Charlie" }),
+			});
+			assert.strictEqual(res.status, 201);
+
+			const res2 = await api.request("/api/users", { method: "GET" });
+			assert.deepStrictEqual(await res2.json(), [
+				{ id: 1, name: "Alice" },
+				{ id: 2, name: "Bob" },
+				{ id: 3, name: "Charlie" },
+			]);
+		});
+
+		await t.test("can PUT a single item", async () => {
+			const res = await api.request("/api/users/1", {
+				method: "PUT",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ id: 3, name: "Charlie" }),
+			});
+			assert.strictEqual(res.status, 204);
+
+			const res2 = await api.request("/api/users", { method: "GET" });
+			assert.deepStrictEqual(await res2.json(), [
+				{ id: 3, name: "Charlie" },
+				{ id: 2, name: "Bob" },
+			]);
+		});
+
+		await t.test("can PATCH a single item", async () => {
+			const res = await api.request("/api/users/1", {
+				method: "PATCH",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ name: "Charlie" }),
+			});
+			assert.strictEqual(res.status, 204);
+
+			const res2 = await api.request("/api/users", { method: "GET" });
+			assert.deepStrictEqual(await res2.json(), [
+				{ id: 1, name: "Charlie" },
+				{ id: 2, name: "Bob" },
+			]);
+		});
+
+		await t.test("can DELETE a single item", async () => {
+			const res = await api.request("/api/users/1", { method: "DELETE" });
+			assert.strictEqual(res.status, 204);
+
+			const res2 = await api.request("/api/users", { method: "GET" });
+			assert.deepStrictEqual(await res2.json(), [{ id: 2, name: "Bob" }]);
+		});
 	});
-
-	// TODO: Test each route's behavior
 });
