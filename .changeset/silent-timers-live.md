290332dfe62a1328063adbae691e759edf214fd2
Shinya Fujino <shf0811@gmail.com>
2024-08-08 21:29:35 +0900

    Add support for Open AI Structured Outputs (#12)
    * Add support for Open AI Structured Outputs
    
    * Create silent-timers-live.md

---
+++ .changeset/silent-timers-live.md
@@ -0,0 +1,5 @@
+---
+"gfa": patch
+---
+
+Add support for Open AI Structured Outputs
--- src/gen.ts
+++ src/gen.ts
@@ -16,6 +16,7 @@ export const AnthropicModelIds = [
 export const OpenAIModelIds = [
 	"gpt-4o",
 	"gpt-4o-2024-05-13",
+	"gpt-4o-2024-08-06",
 	"gpt-4o-mini",
 	"gpt-4o-mini-2024-07-18",
 	"gpt-4-turbo",
@@ -46,7 +47,9 @@ export function createModel(
 	const openai = createOpenAI({
 		apiKey,
 	});
-	return openai(modelId);
+	return openai(modelId, {
+		structuredOutputs: modelId === "gpt-4o-2024-08-06",
+	});
 }
 
 interface GenerateOptions {
